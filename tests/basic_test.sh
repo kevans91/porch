a630397697ac7d161bf06358ba8f1641ba93ae9b
Kyle Evans <kevans@FreeBSD.org>
2024-01-14 21:32:14 -0600

    tests: refactor to reduce new test boilerplate
    Just handle the different test scenarios in one loop, and glob for files in
    the tests/ directory to figure out what we're executing.
    
    Signed-off-by: Kyle Evans <kevans@FreeBSD.org>

--- tests/basic_test.sh
+++ tests/basic_test.sh
@@ -9,76 +9,73 @@ cd "$orchdir"
 
 fails=0
 testid=1
-simple_tests="simple_noecho_fail simple_match simple_nonmatch_fail"
-simple_tests="$simple_tests simple_match_sequence simple_match_sequence_fail"
-simple_tests="$simple_tests simple_match_sequence_twice_fail"
-simple_tests="$simple_tests simple_callback simple_callback_fail"
-simple_tests="$simple_tests simple_fail_handler simple_raw simple_anchored"
-simple_tests="$simple_tests simple_enqueue"
 
-timeout_tests="timeout_basic timeout_global timeout_test"
-
-one_tests="one_basic one_callback_fail"
-
-spawn_tests="spawn_multi spawn_multi_match"
-
-tests="$simple_tests $timeout_tests $one_tests $spawn_tests"
-set -- $tests
+set -- "$scriptdir"/*.orch
 echo "1..$#"
 
-for f in $simple_tests $one_tests $spawn_tests; do
-	testf="$scriptdir/$f.orch"
-
-	if "$orchbin" -f "$testf" -- cat; then
-		res="ok"
-	else
-		fails=$((fails + 1))
-		res="not ok"
-	fi
-
-	echo "$res $testid - $f"
+ok()
+{
+	echo "ok $testid - $f"
 	testid=$((testid + 1))
-done
+}
 
-# Timeout tests are all expected to fail, since we're specifically testing our
-# timeout behavior.
-for f in $timeout_tests; do
+not_ok()
+{
+	msg="$1"
+
+	echo "not ok $testid - $f: $msg"
+	testid=$((testid + 1))
+}
+
+for f in "$@" ;do
+	f=$(basename "$f" .orch)
 	testf="$scriptdir/$f.orch"
-	expected_timeout=$(grep '^-- TIMEOUT:' "$testf" | grep -Eo '[0-9]+')
+	expected_rc=0
 
-	if [ "$expected_timeout" -le 0 ]; then
-		echo "not ok $testid - $f, invalid timeout $expected_timeout"
-		testid=$((testid + 1))
-		continue
-	fi
+	case "$f" in
+	timeout_*)
+		expected_rc=1
+		expected_timeout=$(grep '^-- TIMEOUT:' "$testf" | grep -Eo '[0-9]+')
+		if [ "$expected_timeout" -le 0 ]; then
+			not_ok "invalid timeout $expected_timeout"
+			continue
+		fi
+		;;
+	esac
 
 	start=$(date +"%s")
 	"$orchbin" -f "$testf" -- cat
 	rc="$?"
 	end=$(date +"%s")
 
-	if [ "$rc" -eq 0 ]; then
-		echo "not ok $testid - $f, script matched"
-		testid=$((testid + 1))
+	if [ "$rc" -ne "$expected_rc" ]; then
+		not_ok "expected $expected_rc, exited with $rc"
 		continue
 	fi
 
+	case "$f" in
+	timeout_*)
+		;;
+	*)
+		ok
+		continue
+		;;
+	esac
+
 	elapsed=$((end - start))
 	if [ "$elapsed" -lt "$expected_timeout" ]; then
-		echo "not ok $testid - $f, expected $expected_timeout seconds, finished in $elapsed"
-		testid=$((testid + 1))
+		not_ok "expected $expected_timeout seconds, finished in $elapsed"
 		continue
 	fi
 
 	# Also make sure it wasn't excessively long... this could be flakey.
 	excessive_timeout=$((expected_timeout + 3))
 	if [ "$elapsed" -ge "$excessive_timeout" ]; then
-		echo "not ok $testid - $f, expected $expected_timeout seconds, finished excessively in $elapsed"
-		testid=$((testid + 1))
+		not_ok "expected $expected_timeout seconds, finished excessively in $elapsed"
 		continue
 	fi
 
-	echo "ok $testid - $f"
-	testid=$((testid + 1))
+	ok
 done
+
 exit "$fails"
