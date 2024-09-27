e680446260f62afec197a473aa6c73750c14eec4
Kyle Evans <kevans@FreeBSD.org>
2024-09-26 22:36:07 -0500

    test: minor fixes to libtest.lua
    Normalize PORCHLUA_PATH and PORCHLIB_PATH if they're set but empty.  If we
    don't have either one set, then we don't need to install the package
    searcher in the first place.  Allow only one of them to be set in the
    searcher, though, and fallthrough to the other searchers as appropriate.
    
    Signed-off-by: Kyle Evans <kevans@FreeBSD.org>

--- tests/libtest.lua
+++ tests/libtest.lua
@@ -1,11 +1,18 @@
 local ltest = {}
 
 local libpath = os.getenv("PORCHLIB_PATH")
+if libpath and #libpath == 0 then
+	libpath = nil
+end
+
 local luapath = os.getenv("PORCHLUA_PATH")
+if luapath and #luapath == 0 then
+	luapath = nil
+end
 
 local function porchloader(modname)
 	if not modname:match("^porch") then
-		return nil
+		return
 	end
 
 	local function loadlua(modname, chunk)
@@ -13,48 +20,62 @@ local function porchloader(modname)
 	end
 
 	if modname == "porch" then
+		if not luapath then
+			return
+		end
 		return loadlua, loadfile(luapath .. "/" .. modname .. ".lua", "t")
 	end
 
 	assert(modname:match("^porch."))
 	modname = modname:sub(#"porch." + 1)
 
-	local function lua_path(name)
-		return luapath .. "/porch/" .. name .. ".lua"
-	end
-	local function clib_path(name, ext)
-		return libpath .. "/" .. name .. "/" .. name .. ext
-	end
-	local function try_lua(name)
-		return loadfile(lua_path(name), "t")
+	if luapath then
+		local function lua_path(name)
+			return luapath .. "/porch/" .. name .. ".lua"
+		end
+		local function try_lua(name)
+			return loadfile(lua_path(name), "t")
+		end
+
+		local chunk = try_lua(modname)
+		if chunk then
+			return loadlua, chunk
+		end
 	end
 
-	local chunk = try_lua(modname)
-	if chunk then
-		return loadlua, chunk
+	if libpath then
+		-- Must be a C module
+		local function clib_path(name, ext)
+			return libpath .. "/" .. name .. "/" .. name .. ext
+		end
+		local function try_clib(name, ext)
+			return package.loadlib(clib_path(modname, ext), "luaopen_porch_" ..
+			    modname)
+		end
+
+		local openfunc = try_clib(modname, ".so") or try_clib(modname, ".dylib")
+		if not openfunc then
+			-- luapath is optional, so if we fail here then that could
+			-- mean we're looking at a .lua file that should just fall
+			-- through to the other searchers.
+			return
+		end
+
+		local function loadc(modname, openfunc)
+			return openfunc()
+		end
+
+		return loadc, openfunc
 	end
-
-	-- Must be a C module
-	local function try_clib(name, ext)
-		return package.loadlib(clib_path(modname, ext), "luaopen_porch_" ..
-	    modname)
-	end
-
-	local openfunc = try_clib(modname, ".so") or try_clib(modname, ".dylib")
-	assert(openfunc, "Failed to load " .. modname .. " as either .lua or shlib")
-
-	local function loadc(modname, openfunc)
-		return openfunc()
-	end
-
-	return loadc, openfunc
 end
 
--- We take care to insert our searcher first, otherwise the default set may find
--- system libs before it finds our build products.
-for i = #package.searchers, 1, -1 do
-	package.searchers[i + 1] = package.searchers[i]
+if libpath or luapath then
+	-- We take care to insert our searcher first, otherwise the default set
+	-- may find system libs before it finds our build products.
+	for i = #package.searchers, 1, -1 do
+		package.searchers[i + 1] = package.searchers[i]
+	end
+	package.searchers[1] = porchloader
 end
-package.searchers[1] = porchloader
 
 return ltest
