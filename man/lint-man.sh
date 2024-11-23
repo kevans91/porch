c78c3e370b5e2ec7f226cadecdb06c13f39e6690
Kyle Evans <kevans@FreeBSD.org>
2024-11-23 00:09:39 -0600

    ci: start linting manpages, too
    Add a check-lint target that the check target depends on, and run our custom
    lint-man.sh script as part of that.  This script will check both mandoc and
    igor when installed and escalate any linting errors.
    
    Start installing igor on our FreeBSD image, and bump versions to recently
    supported FreeBSD.
    
    Signed-off-by: Kyle Evans <kevans@FreeBSD.org>

---
+++ man/lint-man.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+if [ ! -d man ]; then
+	1>&2 echo "$0: must be run from the source root"
+	exit 1
+fi
+if ! command -v mandoc >/dev/null; then
+	1>&2 echo "Skipping lint-man; mandoc not found."
+	exit 0
+elif ! command -v igor >/dev/null; then
+	1>&2 echo "Skipping lint-man; igor not found."
+	exit 0
+fi
+
+MANPATH="$PWD"/man:$(manpath)
+export MANPATH
+
+rc=0
+
+igorf=$(mktemp)
+trap 'rc=$?; rm -f "$igorf"; exit $rc' EXIT
+
+for page in man/man[0-9]/*.[0-9]; do
+	if ! mandoc -Tlint -Wstyle "$page"; then
+		rc=1
+		1>&2 echo "ERROR: $page: mandoc -Tlint"
+	fi
+
+	case "$page" in
+	*.5)
+		# We filter out igor's warning about SYNOPSIS for section 5 pages,
+		# because it's not common for them to have a SYNOPSIS.
+		igor -D "$page" | grep -v "SYNOPSIS has not been defined" > "$igorf"
+		;;
+	*)
+		igor -D "$page" > "$igorf"
+		;;
+	esac
+	if [ -s "$igorf" ]; then
+		rc=1
+		1>&2 cat "$igorf"
+		1>&2 echo "ERROR: $page: igor"
+	fi
+done
+
+exit $rc
