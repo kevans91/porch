ea7268fc69ff80697bf0d4505f89641f2470c3de
Kyle Evans <kevans@FreeBSD.org>
2025-05-30 20:06:51 -0500

    tests: enhance sigcheck to examine ignored signals
    We'll use this to test our new sigignore/sigunignore actions.
    
    Signed-off-by: Kyle Evans <kevans@FreeBSD.org>

--- tests/sigcheck.c
+++ tests/sigcheck.c
@@ -13,11 +13,14 @@
 #include <string.h>
 #include <unistd.h>
 
+#define	SA_SIG_IGN	((void (*)(int, siginfo_t *, void *))SIG_IGN)
+
 /*
  * Output various information about the current signal mask.
  *
  * -b: output only the numbers of signals that are currently blocked
  * -c: output only the count of signals that are currently blocked
+ * -i: output only the numbers of signals that are currently ignored
  * normal operation: output the blocked/unblocked status of each signal
  */
 
@@ -35,22 +38,26 @@ usage(int error)
 int
 main(int argc, char *argv[])
 {
-
 	sigset_t sigmask;
-	int ch, error, output = 0;
+	struct sigaction act;
+	int ch, error, nblocked = 0, nignored = 0, output = 0;
 	enum {
 		MODE_NORMAL = 0,
 		MODE_BLOCKEDOUT,
+		MODE_IGNOREDOUT,
 		MODE_COUNT,
 	} curmode = MODE_NORMAL;
 
-	while ((ch = getopt(argc, argv, "bch")) != -1) {
+	while ((ch = getopt(argc, argv, "bchi")) != -1) {
 		switch (ch) {
 		case 'b':
 			curmode = MODE_BLOCKEDOUT;
 			break;
 		case 'c':
 			curmode = MODE_COUNT;
+			break;
+		case 'i':
+			curmode = MODE_IGNOREDOUT;
 			break;
 		case 'h':
 			usage(0);
@@ -61,40 +68,80 @@ main(int argc, char *argv[])
 	}
 
 	if (sigprocmask(SIG_SETMASK, NULL, &sigmask) != 0) {
-		fprintf(stderr, "sigblock: %s\n", strerror(errno));
+		fprintf(stderr, "error: sigblock: %s\n", strerror(errno));
 		return (1);
 	}
 
 	for (int signo = 1; signo < INT_MAX; signo++) {
-		bool blocked;
+		bool blocked, ignored;
 
 		error = sigismember(&sigmask, signo);
 		if (error == -1)
 			break;
 
 		blocked = !!error;
+
+		/*
+		 * Not all signals can be examined, just pretend it's ignored
+		 * if we know the signal is valid.
+		 */
+		if (sigaction(signo, NULL, &act) == -1)
+			ignored = false;
+		else if ((act.sa_flags & SA_SIGINFO) != 0)
+			ignored = act.sa_sigaction == SA_SIG_IGN;
+		else
+			ignored = act.sa_handler == SIG_IGN;
+
+		if (blocked)
+			nblocked++;
+		if (ignored)
+			nignored++;
+
 		switch (curmode) {
 		case MODE_BLOCKEDOUT:
 			if (!blocked)
 				continue;
-			printf("%s%d", output > 0 ? " " : "", signo);
-			output++;
+			printf("%s%d", nblocked > 1 ? " " : "", signo);
+			break;
+		case MODE_IGNOREDOUT:
+			if (!ignored)
+				continue;
+			printf("%s%d", nignored > 1 ? " " : "", signo);
 			break;
 		case MODE_COUNT:
-			if (blocked)
-				output++;
 			break;
 		default:
 			if (blocked)
 				printf("Signal %d is blocked\n", signo);
 			else
 				printf("Signal %d is not blocked\n", signo);
+			if (ignored)
+				printf("Signal %d is ignored\n", signo);
+			else
+				printf("Signal %d is not ignored\n", signo);
 			break;
 		}
 	}
 
-	if (curmode == MODE_COUNT)
-		printf("%d signal%s blocked\n", output, output != 1 ? "s" : "");
-
+	switch (curmode) {
+	case MODE_BLOCKEDOUT:
+		if (nblocked == 0)
+			break;
+		printf("\n");
+		break;
+	case MODE_IGNOREDOUT:
+		if (nignored == 0)
+			break;
+		printf("\n");
+		break;
+	case MODE_COUNT:
+		printf("%d signal%s blocked\n", nblocked,
+		    nblocked != 1 ? "s" : "");
+		printf("%d signal%s ignored\n", nignored,
+		    nignored != 1 ? "s" : "");
+		break;
+	default:
+		break;
+	}
 	return (0);
 }
