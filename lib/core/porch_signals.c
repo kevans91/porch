kevans91/porch

commit 3001ba9732727659b478abf1712f73f80d01af05

lib: core: use signal tables instead of numeric masks
We may have up to 128 signals that we *could* theoretically deal with on
some platforms, so numeric types are a poor fit.  Switch to using a table.

Signed-off-by: Kyle Evans <kevans@FreeBSD.org>

lib/core/porch_signals.c
@@ -15,6 +15,8 @@
 #include <stdbool.h>
 #include <string.h>
 
+#include "porch_lib.h"
+
 #define	SA_SIG_IGN	((void (*)(int, siginfo_t *, void *))SIG_IGN)
 static const char *porch_platform_signames[NSIG];
 
@@ -169,19 +171,23 @@ porch_fetch_sigcaught(sigset_t *sigset)
  * unsetting those bits that are set in the mask.
  */
 void
-porch_mask_apply(bool complement, sigset_t *sigset, int mask)
+porch_mask_apply(bool complement, sigset_t *sigset, const sigset_t *applymask)
 {
-	int error, signo;
+	int error, sigmax;
 
-	while (mask != 0) {
-		signo = ffs(mask);
+	sigmax = porch_sigmax();
+	for (int signo = 1; signo < sigmax; signo++) {
+		error = sigismember(sigset, signo);
+		assert(error != -1);
+		if (!error)
+			continue;
+
 		if (complement)
 			error = sigdelset(sigset, signo);
 		else
 			error = sigaddset(sigset, signo);
 
 		assert(error == 0);
-		mask &= ~(1 << (signo - 1));
 	}
 }
 

lib/core/porch_lua_process.c
@@ -727,28 +727,67 @@ porchlua_process_term_set(porch_ipc_t ipc __unused, struct porch_ipc_msg *msg,
 }
 
 static int
+porch_sigset2table(lua_State *L, const sigset_t *sigset)
+{
+	int ret, sigmax;
+
+	sigmax = porch_sigmax();
+	lua_newtable(L);
+	for (int signo = 1; signo < sigmax; signo++) {
+		ret = sigismember(sigset, signo);
+		assert(ret != -1);
+
+		lua_pushboolean(L, ret);
+		lua_rawseti(L, -2, signo);
+	}
+
+	return (1);
+}
+
+int
+porch_table2sigset(lua_State *L, int idx, sigset_t *sigset)
+{
+	int ret, sigmax;
+
+	assert(idx > 0);
+	sigmax = porch_sigmax();
+	for (int signo = 1; signo < sigmax; signo++) {
+		bool present;
+
+		lua_geti(L, idx, signo);
+		present = lua_toboolean(L, -1);
+		lua_pop(L, 1);
+
+		if (!present)
+			continue;
+		if (sigaddset(sigset, signo) != 0)
+			return (errno);
+	}
+
+	return (0);
+}
+
+static int
 porchlua_process_sigcatch(lua_State *L)
 {
 	struct porch_process *self;
 	struct porch_ipc_msg *msg;
 	struct porch_sigcatch *catchmsg;
 	sigset_t newmask;
-	int error, sigmask;
+	int error;
 	bool catch;
 
 	self = luaL_checkudata(L, 1, ORCHLUA_PROCESSHANDLE);
 	if (lua_gettop(L) < 2 || lua_isnil(L, 2)) {
-		/* Fetch the signal caught mask and return NSIG as well. */
-		lua_pushinteger(L, porch_sigset2mask(&self->sigcaughtmask));
-		lua_pushinteger(L, porch_sigmax());
-		return (2);
+		/* Fetch the signal caught mask in table form. */
+		return (porch_sigset2table(L, &self->sigcaughtmask));
 	}
 
 	catch = lua_toboolean(L, 2);
-	sigmask = luaL_checkinteger(L, 3);
+	luaL_checktype(L, 3, LUA_TTABLE);
 
 	sigemptyset(&newmask);
-	if ((error = porch_mask2sigset(sigmask, &newmask)) != 0) {
+	if ((error = porch_table2sigset(L, 3, &newmask)) != 0) {
 		luaL_pushfail(L);
 		lua_pushstring(L, strerror(error));
 		return (2);
@@ -774,7 +813,7 @@ porchlua_process_sigcatch(lua_State *L)
 	else if (error > 0)
 		return (error);
 
-	porch_mask_apply(!catch, &self->sigcaughtmask, sigmask);
+	porch_mask_apply(!catch, &self->sigcaughtmask, &newmask);
 
 	lua_pushboolean(L, 1);
 	return (1);
@@ -790,23 +829,31 @@ porchlua_process_sigmask(lua_State *L)
 	struct porch_process *self;
 	struct porch_ipc_msg *msg;
 	sigset_t newmask, *sendmask;
-	int error, sigmask;
+	int error;
 
 	self = luaL_checkudata(L, 1, ORCHLUA_PROCESSHANDLE);
 	if (lua_gettop(L) < 2 || lua_isnil(L, 2)) {
-		/* Fetch the signal mask and return NSIG as well. */
-		lua_pushinteger(L, porch_sigset2mask(&self->sigmask));
-		lua_pushinteger(L, porch_sigmax());
-		return (2);
+		/* Fetch the current mask in table form. */
+		return (porch_sigset2table(L, &self->sigmask));
 	}
 
-	sigmask = luaL_checkinteger(L, 2);
-
 	sigemptyset(&newmask);
-	if ((error = porch_mask2sigset(sigmask, &newmask)) != 0) {
-		luaL_pushfail(L);
-		lua_pushstring(L, strerror(error));
-		return (2);
+	if (lua_isinteger(L, 2)) {
+		lua_Number n;
+
+		n = luaL_checkinteger(L, 2);
+		if (n != 0) {
+			luaL_pushfail(L);
+			lua_pushfstring(L, "Expected table or 0, got %d", n);
+			return (2);
+		}
+	} else {
+		luaL_checktype(L, 2, LUA_TTABLE);
+		if ((error = porch_table2sigset(L, 2, &newmask)) != 0) {
+			luaL_pushfail(L);
+			lua_pushstring(L, strerror(error));
+			return (2);
+		}
 	}
 
 	/* Mask was valid, now to apply it if we're not too late. */

lib/porch_lib.h
@@ -120,10 +120,8 @@ int porch_ipc_wait(porch_ipc_t, bool *);
 
 /* porch_signal.c */
 const char * const *porch_signames(size_t *);
-int porch_sigset2mask(const sigset_t *);
-int porch_mask2sigset(int, sigset_t *);
 int porch_fetch_sigcaught(sigset_t *);
-void porch_mask_apply(bool, sigset_t *, int mask);
+void porch_mask_apply(bool, sigset_t *, const sigset_t *);
 int porch_sigmax(void);
 
 /* porch_spawn.c */

share/lua/porch/process.lua
@@ -172,33 +172,26 @@ function Process:raw(is_raw)
 	self.is_raw = is_raw
 	return prev_raw
 end
-local function sigapply(mask, applyfunc, ...)
-	local current_mask = mask
+local function sigapply(mask, applyval, ...)
 	local signals = {...}
+	local changed = false
 
 	local function apply_signals(sigtbl)
 		for _, signo in ipairs(sigtbl) do
 			if type(signo) == "table" then
 				apply_signals(signo)
-			elseif signo > 0 then
-				mask = applyfunc(mask, signo)
+			elseif signo > 0 and mask[signo] ~= applyval then
+				changed = true
+				mask[signo] = applyval
 			end
 		end
 	end
 
 	apply_signals(signals)
-	if mask ~= current_mask then
-		return true, mask
-	end
-
-	return false
+	return changed, mask
 end
 function Process:sigblock(...)
-	local function blocksig(mask, signo)
-		return mask | (1 << (signo - 1))
-	end
-
-	local changed, newmask = sigapply(self:sigmask(), blocksig, ...)
+	local changed, newmask = sigapply(self:sigmask(), true, ...)
 	if changed then
 		return assert(self:sigmask(newmask))
 	end
@@ -206,11 +199,7 @@ function Process:sigblock(...)
 	return true
 end
 function Process:sigunblock(...)
-	local function unblocksig(mask, signo)
-		return mask & ~(1 << (signo - 1))
-	end
-
-	local changed, newmask = sigapply(self:sigmask(), unblocksig, ...)
+	local changed, newmask = sigapply(self:sigmask(), false, ...)
 	if changed then
 		return assert(self:sigmask(newmask))
 	end

commit f73595f3852e759207cdbd0bd2ed1a5ea5157b6e

lib: core: fix porch_mask_apply()
This was meant to apply the bits present in the sigset passed in the third
argument as a diff, not the bits already present in the mask.  This unbreaks
the sigcaughtmask after a sigreset().

Fixes: 3001ba9732727 ("lib: core: use signal tables instead [...]")
Signed-off-by: Kyle Evans <kevans@FreeBSD.org>

lib/core/porch_signals.c
@@ -145,7 +145,7 @@ porch_mask_apply(bool complement, sigset_t *sigset, const sigset_t *applymask)
 
 	sigmax = porch_sigmax();
 	for (int signo = 1; signo < sigmax; signo++) {
-		error = sigismember(sigset, signo);
+		error = sigismember(applymask, signo);
 		assert(error != -1);
 		if (!error)
 			continue;